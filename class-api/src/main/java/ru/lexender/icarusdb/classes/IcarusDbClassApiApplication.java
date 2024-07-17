a4af42737b99cd12143711f180d1cf3133708b2a
LLexender <aleksandrdmitryevich@gmail.com>
2024-07-17 22:39:35 +0300

    Initial commit

---
+++ class-api/src/main/java/ru/lexender/icarusdb/classes/IcarusDbClassApiApplication.java
@@ -0,0 +1,13 @@
+package ru.lexender.icarusdb.classes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcarusDbClassApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IcarusDbClassApiApplication.class, args);
+    }
+
+}
---
+++ class-api/src/test/java/ru/lexender/icarusdb/classes/IcarusDbClassApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lexender.icarusdb.classes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcarusDbClassApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
