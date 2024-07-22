fc6fb03ad3e791cdc203fe547760f8e970050d8b
LLexender <aleksandrdmitryevich@gmail.com>
2024-07-22 21:50:56 +0300

    Added sample template

---
+++ template/src/main/java/ru/lexender/icarusdb/template/IcarusDbTemplateApplication.java
@@ -0,0 +1,17 @@
+package ru.lexender.icarusdb.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * This class is the main class of the application. It is used to start the Spring Boot
+ * application.
+ */
+
+@SpringBootApplication
+public class IcarusDbTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IcarusDbTemplateApplication.class, args);
+    }
+}
---
+++ template/src/test/java/ru/lexender/icarusdb/template/IcarusDbTemplateApplicationTest.java
@@ -0,0 +1,13 @@
+package ru.lexender.icarusdb.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcarusDbTemplateApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
