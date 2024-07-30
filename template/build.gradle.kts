b51b1544017a6d7f8bf612799220d7625d698366
LLexender <aleksandrdmitryevich@gmail.com>
2024-07-30 23:03:51 +0300

    Updated template for further projects

--- template/build.gradle.kts
+++ template/build.gradle.kts
@@ -25,24 +25,17 @@ repositories {
 }
 
 dependencies {
-    // Spring Boot starters
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-cache")
     implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
-
-    // Database and JDBC
     implementation("org.flywaydb:flyway-database-postgresql")
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
     implementation("org.springframework:spring-jdbc")
     runtimeOnly("org.postgresql:postgresql")
     runtimeOnly("org.postgresql:r2dbc-postgresql")
-
-    // API documentation
     implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.6.0")
-
-    // Test dependencies
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.springframework.boot:spring-boot-testcontainers")
     testImplementation("org.testcontainers:db2")
@@ -50,12 +43,8 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
-    // Annotation processing
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
-
-    // Development only dependencies
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     developmentOnly("org.springframework.boot:spring-boot-docker-compose")
 }
