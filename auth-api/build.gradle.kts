9e491f3f1f4089618b9a48ff0466e8ea669063f2
LLexender <aleksandrdmitryevich@gmail.com>
2024-07-30 19:02:57 +0300

    Updated kafka props

--- auth-api/build.gradle.kts
+++ auth-api/build.gradle.kts
@@ -25,45 +25,29 @@ repositories {
 }
 
 dependencies {
-    // Spring Boot starters
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-data-cassandra-reactive")
-
-    // Database
     implementation("org.springframework.kafka:spring-kafka")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.mapstruct:mapstruct:1.5.5.Final")
-
-    // API documentation
     implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.6.0")
-
-    // Security
     implementation("org.springframework.session:spring-session-data-redis")
-
-    // Test dependencies
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
     testImplementation("org.springframework.security:spring-security-test")
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.springframework.kafka:spring-kafka-test")
-    testImplementation("org.springframework.boot:spring-boot-testcontainers")
-    testImplementation("org.testcontainers:cassandra")
-    testImplementation("org.testcontainers:kafka")
     runtimeOnly("com.h2database:h2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
-    // Annotation processing
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.1.0")
     annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
     annotationProcessor("org.projectlombok:lombok")
     compileOnly("org.projectlombok:lombok")
-
-    // Development only dependencies
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     developmentOnly("org.springframework.boot:spring-boot-docker-compose")
 }
